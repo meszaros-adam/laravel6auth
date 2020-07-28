e27697ca364fc9635aae87728997ebdb8069de6a
Mészáros Ádám <meszike91@gmail.com>
2020-07-28 13:22:29 +0200

    9.5 Laravel facades temakorenek tiszatazasa

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -3,12 +3,23 @@
 namespace App\Http\Controllers;
  
 use App\Example;
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
+use Illuminate\Support\Facades\Request;
+use Illuminate\Support\Facades\File;
+use Illuminate\Filesystem\Filesystem;
  
 class PagesController extends Controller
 {
-    public function home(Example $example)
+    public function home(Filesystem $file)
     {
-        ddd(resolve('App\Example'), resolve('App\Example'));
+        //ddd(resolve('App\Example'), resolve('App\Example'));
+        //return view('welcome');
+        //return View::make('welcome');
+        //return request('name');
+        //return Request::input('name');
+        //return File::get(public_path('index.php'));
+        //return \File::get(public_path('index.php'));
+        return $file->get(public_path('index.php'));
     }
 }
